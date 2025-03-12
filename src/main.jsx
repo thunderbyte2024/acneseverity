db8b81e5394a0f62d7d14c5a66f8bf46043a888f
IntelliJens14 <125744017+IntelliJens14@users.noreply.github.com>
2025-03-12 23:17:18 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css"; // Ensure styles are applied
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AcneSeverityPredictor from './components/AcneSeverityPredictor';
+import './styles.css';
 
-const container = document.getElementById("root");
-
-if (container) {
-  const root = ReactDOM.createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("❌ Root element not found! Make sure your HTML has a <div id='root'></div>.");
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AcneSeverityPredictor />
+  </React.StrictMode>
+);
